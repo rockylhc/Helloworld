910d8b98fb2a9bb48832a33092f7f361f00e7ac7
ellen <ellen.su@afusion.com>
2018-03-09 00:09:00 +0800

    aadd

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp'),
+    args = require('yargs').argv,
+    exec = require('child_process').execSync,
+    log = require('fancy-log');
+
+gulp.task('NpmDeploy', function(){
+	exec('npm run deploy', function (err,outlog,errlog){
+		console.log(outlog);
+		console.log(errlog);
+	});
+})
+
+gulp.task('BuildDockerImage',['NpmDeploy'],function(){
+	exec('cat ~/pwd.txt | docker login --username ellensu --password-stdin');
+	exec('docker build -f Dockerfile -t ellensu/nginxweb:' + args.buildversion +' -t ellensu/nginxweb:latest --rm --no-cache .', function (err,outlog,errlog){
+		console.log(outlog);
+		console.log(errlog);
+	});
+})
+
+gulp.task('PushImage',['BuildDockerImage'],function(){
+	exec('docker push ellensu/nginxweb:' + args.buildversion, function (err,outlog,errlog){
+		console.log(outlog);
+		console.log(errlog);
+	});
+	exec('docker push ellensu/nginxweb:latest', function (err,outlog,errlog){
+		console.log(outlog);
+		console.log(errlog);
+	});
+})
+//這段是for play-with-docker特別寫的 與實際server ssh不同
+gulp.task('UpdateDockerCompose',['PushImage'],function() {
+	exec('cd /docker/');
+	exec('ssh ip172-18-0-16-baga5moo6i4000c49q6g@direct.labs.play-with-docker.com');
+	exec('docker-compose pull web','docker-compose up -d --no-deps --build web');
+})
+
+gulp.task('Deploy.stg',['UpdateDockerCompose']);
