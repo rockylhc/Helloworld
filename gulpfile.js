ca06f7a0a55f6c85e5a0ed4e6cccb2a1b2b055d0
ellen <ellen.su@afusion.com>
2018-03-09 12:39:13 +0800

    2

--- gulpfile.js
+++ gulpfile.js
@@ -22,11 +22,11 @@ gulp.task('PushImage',['BuildDockerImage'],function(){
 	});
 })
 //這段是for play-with-docker特別寫的 與實際server ssh不同
-gulp.task('sshserver',['PushImage'],function() {
+gulp.task('SSHServer',['PushImage'],function() {
 	exec('ssh ip172-18-0-20-bagthu0o6i4000bn3qvg@direct.labs.play-with-docker.com');
 })
 
-gulp.task('UpdateDockerCompose',['PushImage'],function() {;
+gulp.task('UpdateDockerCompose',['SSHServer'],function() {;
 	exec('docker-compose pull web','docker-compose up -d --no-deps --build web');
 })
 
