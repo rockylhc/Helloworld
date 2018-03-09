fcda31dc217aff6a6ac2f5d1598233592725b1c2
ellen <ellen.su@afusion.com>
2018-03-09 10:20:02 +0800

    a

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     log = require('fancy-log');
 
 gulp.task('BuildDockerImage',function(){
-	exec('cat /pwd.txt | docker login --username ellensu --password-stdin');
+	exec('cat pwd.txt | docker login --username ellensu --password-stdin');
 	exec('docker build -f Dockerfile -t ellensu/nginxweb:' + args.buildversion +' -t ellensu/nginxweb:latest --rm --no-cache .', function (err,outlog,errlog){
 		console.log(outlog);
 		console.log(errlog);
@@ -24,7 +24,7 @@ gulp.task('PushImage',['BuildDockerImage'],function(){
 //這段是for play-with-docker特別寫的 與實際server ssh不同
 gulp.task('UpdateDockerCompose',['PushImage'],function() {
 	exec('cd /docker/');
-	exec('ssh ip172-18-0-16-baga5moo6i4000c49q6g@direct.labs.play-with-docker.com');
+	exec('ssh ip172-18-0-20-bagthu0o6i4000bn3qvg@direct.labs.play-with-docker.com');
 	exec('docker-compose pull web','docker-compose up -d --no-deps --build web');
 })
 
